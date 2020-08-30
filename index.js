94e8fa69f9b5e3bd7bd79ffe5c24706916d16fc3
DiegoDuranWeb <dduran56@misena.edu.co>
2020-08-30 17:08:55 -0500

    Inicializando proyecto

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+//permite ver las peticiones en la terminal
+import morgan from 'morgan';
+//permite hacer peticiones desde otras pc
+import cors from 'cors';
+import path from 'path';
+import mongoose from 'mongoose';
+//Conexion a la base de datos
+mongoose.Promise= global.Promise;
+const dbUrl='mongodb://localhost:27017/dbsistema'
+mongoose.connect(dbUrl, {useCreateIndex:true, useNewUrlParser: true})
+.then(mongoose=> console.log('Conectando a la BD en el puerto 27017'))
+.catch(err => console.log(err));
+
+
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+//Permitir peticiones json 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+//le indico la carpeta publica
+app.use(express.static(path.join(__dirname ,'public')));
+
+app.set('port',process.env.PORT || 3000);
+
+app.listen(app.get('port'),()=>{
+    console.log('server on ' + app.get('port'));
+    console.log();
+});
