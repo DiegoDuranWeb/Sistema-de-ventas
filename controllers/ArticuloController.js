d669e0fd8c241fd306ebbec71acd03c61674b878
DiegoDuranWeb <dduran56@misena.edu.co>
2020-09-01 21:28:17 -0500

    modulo bankend articulo termidado

--- controllers/ArticuloController.js
+++ controllers/ArticuloController.js
@@ -3,7 +3,7 @@ import models from '../models';
 export default{
     add: async(req, res, next) =>{
         try {
-            const registro =await models.Categoria.create(req.body);
+            const registro =await models.Articulo.create(req.body);
             res.status(200).json(registro);
         } catch (error) {
             res.status(500).send({
@@ -15,7 +15,8 @@ export default{
 
     query: async(req, res, next) =>{
         try {
-            const registro = await models.Categoria.findOne({_id:req.query._id});
+            const registro = await models.Articulo.findOne({_id:req.query._id})
+            .populate('articulo', {nombre:1});
             if(!res){
                 res.status(404).send({
                     message: 'El registro no existe'
@@ -34,8 +35,8 @@ export default{
     list:async(req, res, next) =>{
         try {
             let valor= req.query.valor;
-
-            const registro = await models.Categoria.find({$or:[{'nombre':new RegExp(valor,'i')},{'descripcion':new RegExp(valor,'i')}]},{createdAt:0})
+            const registro = await models.Articulo.find({$or:[{'nombre':new RegExp(valor,'i')},{'descripcion':new RegExp(valor,'i')}]},{createdAt:0})
+            .populate('categoria', {nombre:1})
             .sort({'createdAt':-1});
             res.status(200).json(registro);
         } catch (error) {
@@ -47,10 +48,15 @@ export default{
     },
     update: async(req, res, next) =>{
         try {
-            const registro = await models.Categoria.findByIdAndUpdate(
+            const registro = await models.Articulo.findByIdAndUpdate(
                 {   _id:req.body._id },
-                {   nombre:req.body.nombre,
-                    descripcion:req.body.descripcion});
+                {   categoria:req.body.categoria,
+                    codigo:req.body.codigo,
+                    nombre:req.body.nombre,
+                    descripcion:req.body.descripcion,
+                    precio_venta:req.body.precio_venta,
+                    stock:req.body.stock,
+                    estado:req.body.estado});
             res.status(200).json(registro);
         } catch (error) {
             res.status(500).send({
@@ -61,7 +67,7 @@ export default{
     },
     remove: async(req, res, next) =>{
         try {
-            const registro= await models.Categoria.findByIdAndDelete({_id:req.body._id});
+            const registro= await models.Articulo.findByIdAndDelete({_id:req.body._id});
             res.status(200).json(registro);
         } catch (error) {
             res.status(500).send({
@@ -72,7 +78,7 @@ export default{
     },
     activate: async(req, res, next) =>{
         try {
-            const registro = await models.Categoria.findByIdAndUpdate({_id:req.body._id},{estado:1});
+            const registro = await models.Articulo.findByIdAndUpdate({_id:req.body._id},{estado:1});
             res.status(200).json(registro);
             
         } catch (error) {
@@ -84,7 +90,7 @@ export default{
     },
     deactivate: async(req, res, next) =>{
         try {
-            const registro = await models.Categoria.findByIdAndUpdate({_id:req.body._id},{estado:0});
+            const registro = await models.Articulo.findByIdAndUpdate({_id:req.body._id},{estado:0});
             res.status(200).json(registro);
             
         } catch (error) {
