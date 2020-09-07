c77ca2a9a4eef73ed5b50fc7b742f63952d49623
DiegoDuranWeb <dduran56@misena.edu.co>
2020-09-07 08:38:46 -0500

    backend proveedores y clientes terminado

--- models/index.js
+++ models/index.js
@@ -1,10 +1,13 @@
 import Categoria from './categoria';
 import Articulo from './articulo';
 import Usuario from './usuario';
+import Persona from './persona';
+
 
 
 export default{
     Categoria,
     Articulo,
-    Usuario
+    Usuario,
+    Persona
 }
